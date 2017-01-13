9b41354294522390098b8d4b291af44dfa972a7d
_Slade <slade8@gmail.com>
2017-01-13 18:32:51 -0500

    Implement random insults
    The bot insults users who messes up its commands, choosing a random
    insult each time. Previously, there was only one placeholder insult
    that was hardcoded. This commit adds the functionality to load insults
    from an insults file specified in the config as `insults_file`. See the
    docstring for `random_insult()` for details.

--- dragonbot.py
+++ dragonbot.py
@@ -6,6 +6,7 @@ from __future__ import (
 )
 import argparse
 import asyncio
+import codecs
 import collections
 import discord
 import functools
@@ -138,11 +139,39 @@ def version():
     )
 
 def random_insult():
-    insults = [
-        '<insults go here>'
-    ]
+    '''
+    Random insults that the bot calls people who fail to use its
+    commands properly.
+
+    These are loaded from a JSON file specified by the `insults_file` in
+    `config.json`. They should be given as an object with two fields:
+    `encoding`, which optionally specifies the encoding of the insults
+    (a parameter to `codecs.decode`), and `insults`, which is an array
+    containing the insults.
+
+    The `encoding` field is to allow obfuscation of the insults, e.g.
+    with `rot_13`. The strings themselves must be encoded as UTF-8 text,
+    in accordance with RFC 7159.
+    '''
+    if not hasattr(random_insult, '_cache'):
+        if 'insults_file' not in config:
+            raise FileNotFoundError('Could not find insults file in config')
+        else:
+            with open(config['insults_file'], 'r', encoding='utf-8') as fh:
+                obj = json.load(fh)
+            if 'insults' not in obj:
+                raise ValueError(
+                    'Malformed insults object, expected "insults" field'
+                )
+            insults = obj['insults']
+            if 'encoding' in obj:
+                insults = [
+                    codecs.decode(insult, obj['encoding'])
+                        for insult in obj['insults']
+                ]
+            random_insult._cache = insults
     stats['insults picked'] += 1
-    return random.choice(insults)
+    return random.choice(random_insult._cache)
 
 def chunker(seq, size):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
