7560d3ce564a34cd6cdf0bbfa5e657d39eb86cf0
_Slade <slade8@gmail.com>
2017-01-17 21:34:35 -0500

    Refactor options handling
    This removes the `--debug` option and instead splits the functionality
    of `--log` between `--log` and `--global-log`.

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
-import unicodedata
+import logging
 import sys
+import unicodedata
 
 def chunker(seq, size):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
@@ -7,6 +8,14 @@ def chunker(seq, size):
 def not_none(value, default):
     return value if value is not None else default
 
+def get_log_level(level_str):
+    try:
+        level = getattr(logging, level_str)
+        if not isinstance(level, int):
+            raise AttributeError('Dummy')
+        return level
+    except AttributeError:
+        return None
 
 def remove_punctuation(text):
     if not hasattr(remove_punctuation, '_tbl'):
