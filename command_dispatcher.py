3bc7f75e72f54944bfaff83778d26d3d57033a54
_Slade <slade8@gmail.com>
2017-01-18 19:04:48 -0500

    Work on command dispatcher

---
+++ command_dispatcher.py
@@ -0,0 +1,45 @@
+from collections import namedtuple
+
+class CommandDispatcher(object):
+    """Dispatches bot commands, which can be registered by other
+    modules. Handles permissions.
+    """
+
+    Command = namedtuple('Command', ['name', 'func', 'may_use'])
+
+    def __init__(self, client, read_only=False):
+        """Construct a new CommandDispatcher bound to the given client.
+
+        Arguments:
+            client -- A discord.Client.
+            read_only -- Whether r/w commands can be executed. Defaults to
+                False.
+        """
+        self.commands = {}
+        self.client = client
+
+    def register_command(self, command_name, command_func, may_use):
+        """Register a command to make it known to the dispatcher.
+
+        Arguments:
+            command_name -- The name of the command, which is the
+                that is dispatched on.
+            command_func -- The function to call. It should take a
+                discord.Client client, a discord.Message, and a
+                string (the argument(s) to the message) as positional
+                parameters.
+            may_use -- An object containing the IDs of users who are
+                permitted to use this function. The collection must
+                implement the __contains__ method.
+        """
+        if command_name in commands:
+            raise RuntimeError('Command already registered')
+        self.commands[command_name] = Command(
+            name=command_name,
+            func=command_func,
+            may_use=may_use
+        )
+
+    def dispatch(self, command_name, message):
+        if command_name in commands:
+
